48a0f75e80c93f700b25966f6aec0e76664ea210
BenKephart <BenKephart@users.noreply.github.com>
2016-04-01 09:27:43 -0400

    Added files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Console.ReadKey().ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
